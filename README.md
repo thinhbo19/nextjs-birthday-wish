99b18824e25cbe5e4c830f92b398f6d15bf4fd5a
thinhbo19 <111267094+thinhbo19@users.noreply.github.com>
2024-11-06 23:11:29 +0700

    firts

---
+++ README.md
@@ -0,0 +1,99 @@
+# Happy Birthday Wisher
+
+Check out the [Live Website](https://hbdwish.ml).
+
+This is a simple [Next.js](https://nextjs.org/) project where you can generate a birthday wish for someone.
+
+Just enter their name, press Go and share them the generated link :)
+
+Don't forget to ⭐️ this repository!
+
+## Images
+
+![Happy Birthday Wisher Home Page](https://user-images.githubusercontent.com/46792249/136275729-454a4475-18fc-4b41-af98-dbf781778b9e.png)
+
+## Local Setup
+
+First, clone this repository:
+
+```bash
+git clone https://github.com/gouravkhunger/nextjs-birthday-wisher
+```
+
+Install the dependencies:
+
+```bash
+npm install
+```
+
+Then, run the development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+
+## Contributing
+In order to make a hassle-free environment, please make sure to read the [Contribution Guidelines](CONTRIBUTING.md) and follow all the steps mentioned there.
+Are we missing any of your favorite features, which you think you can add to it❓ Feel free to do so. <br />
+
+All contributions are welcome.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+
+## Special Thanks
+
+I would like to show my sincere gratitude to these amazing people for making this simple project a big deal. 
+
+Without their contributions, this project wouldn't be as good as it is right now.
+
+- [@PhattOZ](https://github.com/PhattOZ) <br />
+    Major [contribution](https://github.com/gouravkhunger/nextjs-birthday-wish/commits?author=PhattOZ): Super awesome copy wish link, wavy animation, style fixes.
+
+- [@adarshzpatel](https://github.com/adarshzpatel) <br />
+    Major [contribution](https://github.com/gouravkhunger/nextjs-birthday-wish/commits?author=adarshzpatel): Multicolour theme.
+
+- [@Sejal-16](https://github.com/Sejal-16) <br />
+    Major [contribution](https://github.com/gouravkhunger/nextjs-birthday-wish/commits?author=Sejal-16): Birthday quotes idea, background music.
+
+Checkout [all the contributors](https://github.com/gouravkhunger/nextjs-birthday-wish/graphs/contributors).
+
+## Credits
+
+<div>Favicon made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>.</div>
+
+## License
+
+```
+MIT License
+
+Copyright (c) 2021 Gourav Khunger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
---
+++ pages/index.js
@@ -0,0 +1,157 @@
+import React, { useState, useEffect, useRef } from "react";
+import Head from "next/head";
+import styles from "../styles/Name.module.css";
+import ConfettiGenerator from "confetti-js";
+import messages from "../utils/birthdayWishes";
+import useTheme from "../hooks/useTheme";
+import * as htmlToImage from "html-to-image";
+import FileSaver from "file-saver";
+import Image from "next/image";
+
+const Home = () => {
+  const name = "Phú";
+  const color = 0;
+
+  const [downloading, setDownloading] = useState(false);
+  const [downloadedOnce, setDownloadedOnce] = useState(false);
+  const audioRef = useRef();
+  const [isClient, setIsClient] = useState(false); // Kiểm tra xem đang chạy trên client
+
+  const { setTheme } = useTheme();
+
+  useEffect(() => {
+    setIsClient(true); // Thiết lập trạng thái khi đang ở phía client
+
+    setTheme(color);
+
+    if (downloading === false) {
+      const confettiSettings = {
+        target: "canvas",
+        start_from_edge: true,
+      };
+      const confetti = new ConfettiGenerator(confettiSettings);
+      confetti.render();
+      audioRef.current.play();
+    }
+  }, [color, downloading]);
+
+  useEffect(() => {
+    if (downloading === true && downloadedOnce === false) {
+      downloadImage();
+    }
+  }, [downloading, downloadedOnce]);
+
+  const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+  const downloadImage = () => {
+    if (downloadedOnce === true) return;
+
+    const node = document.getElementById("image");
+
+    if (node) {
+      setDownloadedOnce(true);
+
+      htmlToImage.toPng(node).then((blob) => {
+        FileSaver.saveAs(blob, "birthday-Home.png");
+        setDownloading(false);
+      });
+    }
+  };
+
+  const title = (name) => {
+    const Home = "Happy Birthday " + name + "!";
+    const base_letters = [];
+    const name_letters = [];
+
+    for (let i = 0; i < Home.length; i++) {
+      if (i < 15) {
+        const letter = Home.charAt(i);
+        base_letters.push(
+          <span key={i} style={{ "--i": i + 1 }}>
+            {letter}
+          </span>
+        );
+      } else {
+        const letter = Home.charAt(i);
+        name_letters.push(
+          <span key={i} style={{ "--i": i + 1 }} className={styles.span}>
+            {letter}
+          </span>
+        );
+      }
+    }
+
+    return (
+      <>
+        {downloading ? (
+          <h1
+            className={styles.titleImg}
+            style={{ "--Home-length": Home.length }}
+          >
+            <div>{base_letters.map((letter) => letter)}</div>
+            <div>{name_letters.map((letter) => letter)}</div>
+          </h1>
+        ) : (
+          <h1 className={styles.title} style={{ "--Home-length": Home.length }}>
+            <div>{base_letters.map((letter) => letter)}</div>
+            <div>{name_letters.map((letter) => letter)}</div>
+          </h1>
+        )}
+      </>
+    );
+  };
+
+  if (downloading) {
+    return (
+      <div className={styles.containerImg} id="image" onClick={downloadImage}>
+        {downloadImage()}
+        <main className={styles.image}>
+          <div>
+            <div className={styles.main}>{title(name)}</div>
+
+            <div style={{ height: 40 }} />
+
+            <p className={styles.descImg}>
+              {messages[randomNumber(0, messages.length)].value}
+            </p>
+          </div>
+        </main>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Happy Birthday {name}</title>
+        <meta name="description" content={`A surprise birthday Home!`} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <canvas className={styles.canvas} id="canvas"></canvas>
+
+      <main className={styles.animate}>
+        <div className={styles.imageContainer}>
+          <Image src="/1.jpg" width={300} height={300} alt="phu"></Image>
+        </div>
+
+        <div>
+          <div className={styles.main}>{title(name)}</div>
+          <p className={styles.desc}>
+            {messages[randomNumber(0, messages.length)].value}
+          </p>
+        </div>
+
+        {/* Render ảnh nhỏ */}
+      </main>
+
+      <audio ref={audioRef} id="player" autoPlay>
+        <source src="media/hbd.mp3" />
+      </audio>
+    </div>
+  );
+};
+
+export default Home;
